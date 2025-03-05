694c1efc0dcbc8766a30d25fce82af4d242f2367
Cedrik Campeau <cedrik.campeau@gmail.com>
2025-03-05 16:52:04 -0500

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // IMPORTANT : Ajoute la propriété "base" pointant vers ton repo GitHub Pages.
+  base: '/CC_CONSULTANT/',
+
   plugins: [react()],
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
+
